be8f6f5a828da600d277df2c18052c3e770a07ff
xeiv-v <evitaadk01@gmail.com>
2026-01-02 19:24:47 +0700

    Fix 419 Laravel 12 Railway HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function ($middleware) {
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
+    })
+    ->withExceptions(function ($exceptions) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+    ->create();
